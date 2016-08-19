65f942af4925f28f03ab5960f3ac51d95bb8921a
Mnrikard <mnrikard@gmail.com>
2016-08-19 16:26:05 -0400

    reworked with canvas

--- gameboard.js
+++ gameboard.js
@@ -33,7 +33,7 @@ gameengine.prototype.createRandomVertWalls = function(wallarray, possibles, wall
 };
 
 gameengine.prototype.randomWalls = function(wallarray) {
-	var wallPercent = 0.2*wallarray.length;
+	var wallPercent = 0.2*Math.pow(wallarray.length,2);
 
 	var walLen = 1;
 	var walsBuilt = 0;
@@ -54,6 +54,9 @@ gameengine.prototype.defineBoard = function(sq){
 	this.board.hwalls = [];
 	this.board.vwalls = [];
 
+	var size = 16;
+	this.board.tileSize = size;
+
 	if(sq < 1){
 		alert("why you no define larger board?");
 		return;
@@ -62,7 +65,12 @@ gameengine.prototype.defineBoard = function(sq){
 	for(var r=0;r<sq;r++) {
 		this.board.tiles.push({rowid:r,columns:[]});
 		for(var c=0;c<sq;c++){
-			this.board.tiles[r].columns.push({colid:c,contents:null});
+			this.board.tiles[r].columns.push({
+				colid:c,
+				contents:null,
+				x:(size*c)+c+1,
+				y:(size*r)+r+1
+			});
 		}
 	}
 
@@ -74,10 +82,24 @@ gameengine.prototype.defineBoard = function(sq){
 		}
 		for(var cn=0;cn<sq;cn++) {
 			if(rn < sq-1) {
-				this.board.hwalls[rn].walls.push({walid:cn,blocked:false});
+				this.board.hwalls[rn].walls.push({
+					walid:cn,
+					blocked:false,
+					x1:size*cn,
+					x2:size*(cn+1),
+					y1:(rn+1)*size,
+					y2:(rn+1)*size
+				});
 			}
 			if(cn < sq-1) {
-				this.board.vwalls[rn].walls.push({walid:cn,blocked:false});
+				this.board.vwalls[rn].walls.push({
+					walid:cn,
+					blocked:false,
+					x1:size*(1+cn),
+					x2:size*(1+cn),
+					y1:rn*size,
+					y2:(rn+1)*size
+				});
 			}
 		}
 	}
@@ -90,39 +112,41 @@ gameengine.prototype.clearBoard = function() {
 	this.board.tiles = [];
 	this.board.hwalls = [];
 	this.board.vwalls = [];
+};
 
-	while(gameboard.firstChild) {
-		gameboard.removeChild(gameboard.firstChild);
+gameengine.prototype.drawCanvas = function() {
+	var ctx = gamecanvas.getContext("2d");
+
+	ctx.fillStyle = "#cccccc";
+	var maxSize = ((this.board.tileSize+1)*(this.board.tiles.length))+1;
+	ctx.fillRect(0,0,maxSize,maxSize);
+	
+	ctx.fillStyle = "#ffffff";
+	for(var r=0; r<this.board.tiles.length; r++) {
+		for(var c=0;c<this.board.tiles[r].columns.length;c++){
+			var cell = this.board.tiles[r].columns[c];
+			ctx.fillRect(cell.x,cell.y,this.board.tileSize,this.board.tileSize);
+		}
 	}
+
+	ctx.fillStyle = "#000000";
+
+	this.drawWall(ctx, this.board.hwalls);
+	this.drawWall(ctx, this.board.vwalls);
 };
 
-gameengine.prototype.addTileRow = function(row){
-	var curRow = dom.createElement("div");
-	curRow.id = "row"+row;
-	curRow.className = "row";
-	gameboard.appendChild(curRow);
-	return curRow;
-};
-
-gameengine.prototype.addTile = function(column, rowindex, colindex) {
-	var tile = dom.createElement("div");
-	tile.className="tile";
-	tile.id = "tiler"+rowindex+"c"+colindex;
-	if(column.contents) {
-		tile.innerHTML = column.contents;
-	} else {
-		tile.innerHTML = "&nbsp;";
+gameengine.prototype.drawWall = function(ctx, wallarray) {
+	for(var wr=0;wr<wallarray.length;wr++) {
+		for(var wc=0;wc<wallarray[wr].walls.length;wc++) {
+			var wall = wallarray[wr].walls[wc];
+			if(wall.blocked) {
+				ctx.moveTo(wall.x1,wall.y1);
+				ctx.lineTo(wall.x2,wall.y2);
+				ctx.stroke();
+			}
+		}
 	}
-	gameboard.appendChild(tile);
-};
-
-gameengine.prototype.addWall = function(wall, row, col, classNm) {
-	var wall = dom.createElement("div");
-	wall.className = classNm;
-	wall.id = "wallr"+row+"c"+col;
-	wall.innerHTML = "&nbsp;";
-	gameboard.appendChild(wall);
-};
+}
 
 gameengine.prototype.drawBoard = function() {
 	this.clearBoard();
@@ -130,22 +154,7 @@ gameengine.prototype.drawBoard = function() {
 	var sq = parseInt(square.value);
 	this.defineBoard(sq);
 
-	for(var row=0;row<this.board.tiles.length;row++) {
-		var curRow = this.addTileRow(row);
-
-		for(var col=0;col < this.board.tiles[row].columns.length;col++){
-			this.addTile(this.board.tiles[row].columns[col],row,col);
-			if(col < this.board.vwalls[row].walls.length) {
-				this.addWall(this.board.vwalls[row].walls[col], row, col, "hwall");
-			}
-		}
-
-		if(row < this.board.hwalls.length) {
-			for(col=0;col < this.board.hwalls[row].walls.length;col++) {
-				this.addWall(this.board.hwalls[row].walls[col], row, col, "vwall");
-			}
-		}
-	}
+	this.drawCanvas();
 };
 
 var game = new gameengine();
